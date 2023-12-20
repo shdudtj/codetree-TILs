1a1135db441aaf0081c1ea9601171d8e32d50e56
shdudtj <shdudtj902@gmail.com>
2023-12-21 07:17:06 +0900

    Title: 문자열 받아 출력, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 231221/문자열 받아 출력/enter-word-and-print.java
+++ 231221/문자열 받아 출력/enter-word-and-print.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        string s;
+        String s;
         s = sc.next();
         System.out.print(s);
     }
