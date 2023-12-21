bfc40b41d103e102c92b1030d268e9158105626c
shdudtj <shdudtj902@gmail.com>
2023-12-21 23:11:15 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231221/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        int l = sc.nextInt();
+        w += 8;
+        l *= 3;
+        System.out.println(w);
+        System.out.println(l);
+        System.out.println(w*l);
+    }
+}
