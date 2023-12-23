145c70131af447f6b8a82026870dc754653254ee
shdudtj <shdudtj902@gmail.com>
2023-12-23 15:14:40 +0900

    Title: 시험 통과 여부 확인하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231223/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n >= 80) {
+            System.out.print("pass");
+        }
+        else {
+            System.out.print((80-n)+ " more score");
+        }
+    }
+}
