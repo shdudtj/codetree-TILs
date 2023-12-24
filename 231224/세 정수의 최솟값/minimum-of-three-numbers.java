26e69da396d5581c18776e2075363cf418383fb5
shdudtj <shdudtj902@gmail.com>
2023-12-24 22:34:47 +0900

    Title: 세 정수의 최솟값, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 231224/세 정수의 최솟값/minimum-of-three-numbers.java
+++ 231224/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -5,10 +5,10 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        if (a < b && a < c) {
+        if (a <= b && a <= c) {
             System.out.print(a);
         }
-        else if (b < a && b < c) {
+        else if (b <= a && b <= c) {
             System.out.print(b);
         }
         else {
