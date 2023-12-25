1386335db92115e35b35088a831410aedab7f0a6
shdudtj <shdudtj902@gmail.com>
2023-12-25 16:46:35 +0900

    Title: 윤년인가, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231225/윤년인가/is-leap-year.java
+++ 231225/윤년인가/is-leap-year.java
@@ -3,11 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
-        if (y % 4 == 0 || (y % 4 == 0) && (y % 100 == 0) && (y % 400 == 0)) {
+        if (y % 4 == 0) {
             System.out.print("true");
         }
         else if (y % 4 == 0 && y % 100 == 0) {
             System.out.print("false");
+        }
+        else if ((y % 4 == 0) && (y % 100 == 0) && (y % 400 == 0)) {
+            System.out.print("true");
         }
         else {
             System.out.print("false");
