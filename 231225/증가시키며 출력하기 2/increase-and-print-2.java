b29320fab4657ef15911860a7c904325589aea19
shdudtj <shdudtj902@gmail.com>
2023-12-25 19:57:17 +0900

    Title: 증가시키며 출력하기 2, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231225/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i < 27) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
