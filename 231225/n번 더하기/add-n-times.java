7ecebc666e6fc1399cf88bfc519848b53043ad4e
shdudtj <shdudtj902@gmail.com>
2023-12-25 20:45:23 +0900

    Title: n번 더하기, Time: 172ms, Memory: 10MB, Status: Passed - CodeTree

--- 231225/n번 더하기/add-n-times.java
+++ 231225/n번 더하기/add-n-times.java
@@ -4,10 +4,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int n = sc.nextInt();
-        int i = a+n;
-        while (i <= 7*n ) {
-            System.out.println(i);
-            i += n;
+        for (int i = 1; i < n+1 ; i++) {
+            System.out.println((a + (n*i)));
         }
     }
 }
