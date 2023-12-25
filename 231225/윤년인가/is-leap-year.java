4339e1acd380e180490b9abda35d76fcca1ec593
shdudtj <shdudtj902@gmail.com>
2023-12-25 16:44:40 +0900

    Title: 윤년인가, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231225/윤년인가/is-leap-year.java
+++ 231225/윤년인가/is-leap-year.java
@@ -6,6 +6,9 @@ public class Main {
         if (y % 4 == 0 || (y % 4 == 0) && (y % 100 == 0) && (y % 400 == 0)) {
             System.out.print("true");
         }
+        else if (y % 4 == 0 && y % 100 == 0) {
+            System.out.print("false");
+        }
         else {
             System.out.print("false");
         }
