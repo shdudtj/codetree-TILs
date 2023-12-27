b2b89dc7e47bf60027e6afb277acfe3e4e612927
shdudtj <shdudtj902@gmail.com>
2023-12-27 18:25:44 +0900

    Title: 점수 비교 2, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231227/점수 비교 2/score-comparison-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int b2 = sc.nextInt();
+        if (a1 > b1 && a2 > b2){
+            System.out.print(1);
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
