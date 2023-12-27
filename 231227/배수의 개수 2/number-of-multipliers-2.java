7730da31ae046d5261b0db1824b13ddce8fed8bc
shdudtj <shdudtj902@gmail.com>
2023-12-27 11:55:24 +0900

    Title: 배수의 개수 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231227/배수의 개수 2/number-of-multipliers-2.java
+++ 231227/배수의 개수 2/number-of-multipliers-2.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int cnt = 0;
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             if (i % 2 != 0) {
                 cnt++;
             }
