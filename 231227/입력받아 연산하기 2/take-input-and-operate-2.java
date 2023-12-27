49640427023962954e8c72bae66c02019efb8f5a
shdudtj <shdudtj902@gmail.com>
2023-12-27 18:12:54 +0900

    Title: 입력받아 연산하기 2, Time: 183ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231227/입력받아 연산하기 2/take-input-and-operate-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + 894;
+        int d = b/10;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
